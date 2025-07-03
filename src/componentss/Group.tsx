22fe7f0d2f418be9e08291c30f00c7b12dd5d532
Sudhansu <147788967+hihellosuper@users.noreply.github.com>
2025-07-03 12:10:10 +0530

    Update Group.tsx

--- src/componentss/Group.tsx
+++ src/componentss/Group.tsx
@@ -1,19 +1,1 @@
-// // import { Fragment } from "react/jsx-runtime";
 
-// function Group () {
-// let items = ["Food", "Grocery", "Shopping"];
-// items = [];
-//     return (
-//         <>
-//             <h1>List</h1>
-//             {items.length === 0 ? <p>Nothing</p> : null}
-//             <ul className="groups" >
-//                 {items.map((item) => (
-//                     <li key={item}> {item} </li>
-//                 ))}
-//             </ul>
-//         </>
-//     );
-// }
-
-// export default Group;
